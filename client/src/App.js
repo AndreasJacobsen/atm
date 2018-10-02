8936c8ff3dda9cf362096448bd59a7e7cd18d0d5
Andreas Jacobsen <andreas1300@gmail.com>
2018-10-02 12:10:35 +0200

    removed boileplate code from create-react-app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -25,10 +24,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">{this.state.response}</p>
       </div>
     );
