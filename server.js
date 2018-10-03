279fdee23482f3bb699820fe8952fd10273897da
Andreas Jacobsen <andreas1300@gmail.com>
2018-10-03 15:52:56 +0200

    mongodb klarer nå lagre data fra skjemaet

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/atm");
+
+const whitdrawalSchema = new mongoose.Schema({
+  name: String,
+  whitdrawal: Number,
+  reason: String
+});
+var AtmUser = mongoose.model("AtmUser", whitdrawalSchema);
 
 app.use(
   bodyParser.urlencoded({
@@ -12,9 +22,19 @@ app.use(
 
 app.use(bodyParser.json());
 app.post("/api/formdata", function(req, res) {
-  var name = req.body.name; //fixed
-  var whitdrawal = req.body.whitdrawal; //fixed
-  var reason = req.body.reason; //fixed
+  const whitdrawalData = new AtmUser(req.body);
+  whitdrawalData
+    .save()
+    .then(item => {
+      res.send("item saved to database");
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
+
+  const name = req.body.name;
+  const whitdrawal = req.body.whitdrawal;
+  const reason = req.body.reason;
   console.log("Hentet data", name, whitdrawal, reason);
 });
 
