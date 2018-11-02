afefda00a2a00af40cb606ba785566bda403ec39
Andreas Jacobsen <andreas1300@gmail.com>
2018-11-02 10:51:40 +0100

    sender nå korrekt statuskode

--- controller/authenticator.js
+++ controller/authenticator.js
@@ -8,7 +8,7 @@ router.post('/', function(req, result) {
   const CardNumber = req.body.cardnumber;
   const pin = req.body.pin;
   connection.query(
-    'SELECT PIN, CardNumber FROM userCards WHERE CardNumber = ?',
+    'SELECT PIN, CardNumber, UserID FROM userCards WHERE CardNumber = ?',
     [CardNumber],
     function(error, results, fields) {
       if (error) {
@@ -17,37 +17,37 @@ router.post('/', function(req, result) {
           code: 400,
           failed: 'error ocurred'
         });
-      } else {
+      } else if (results.length > 0) {
         console.log('The result is: ', results);
         console.log('handling pin', pin);
-        if (results.length > 0) {
-          console.log('results over 0 is true');
-          console.log('pin result from query is: ', results[0].PIN);
-          console.log('pin result from form is: ', pin);
+        console.log('results over 0 is true');
+        console.log('pin result from query is: ', results[0].PIN);
+        console.log('pin result from form is: ', pin);
 
-          bcrypt.compare(pin, results[0].PIN, function(err, res) {
-            if (res) {
-              console.log('login succefull');
-              result.send({
-                code: 200,
-                success: 'login sucessfull'
-              });
-            } else if (err) {
-              console.log('something went wrong/email and password does not match');
-              console.log(err);
-              result.send({
-                code: 204,
-                success: err
-              });
-            }
-          });
-        } else {
-          console.log('result lenght is 0');
-          result.send({
-            code: 204,
-            success: 'Email does not exits'
-          });
-        }
+        bcrypt.compare(pin, results[0].PIN, function(err, res) {
+          if (res) {
+            console.log('login succefull');
+            result.status(200);
+            result.send('Correct username and password');
+          } else if (res == false) {
+            console.log('something went wrong/email and password does not match');
+            console.log(err);
+            result.status(204);
+            result.send('wrong username or password');
+          } else {
+            console.log('result lenght is 0');
+            result.send({
+              code: 204,
+              success: 'Email does not exits'
+            });
+          }
+        });
+      } else {
+        console.log('result lenght is 0');
+        result.send({
+          code: 204,
+          success: 'Email does not exits'
+        });
       }
     }
   );
